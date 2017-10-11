862696873b6859d05b70a509dbe1fada274360a0
[banYingGit] <[635221846@qq.com]>
2017-10-11 10:00:28 +0800

    市集建页面

--- ionic3App/src/pages/market/market.module.ts
+++ ionic3App/src/pages/market/market.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { MarketPage } from './market';
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {MarketPage} from './market';
 
 @NgModule({
   declarations: [
@@ -10,4 +10,15 @@ import { MarketPage } from './market';
     IonicPageModule.forChild(MarketPage),
   ],
 })
-export class MarketPageModule {}
+export class MarketPageModule {
+  itemsObj: Array<ObjectModule>;
+  itemsList: Array<ObjectModule>;
+  items: Array<ObjectModule>
+}
+export class ObjectModule {
+  proImg: string;
+  title: string;
+  id: string;
+  price: string;
+  priceOld: string;
+}
--- ionic3App/src/pages/market/market.ts
+++ ionic3App/src/pages/market/market.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams, LoadingController} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams, LoadingController, Slides} from 'ionic-angular';
 import {MarketService} from './market.service';
+
+import {GoodsDetailsPage} from '../goods-details/goods-details';
 /**
  * Generated class for the MarketPage page.
  *
@@ -15,7 +17,14 @@ import {MarketService} from './market.service';
 })
 export class MarketPage {
 
+  @ViewChild(Slides) slides: Slides;
+  tabBarData = [{}];
+  slidesImg = []
+  listData = []
   loading: any;
+  isTabActive = ''
+  pageNum = 1;
+  isShowSlide = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public marketService: MarketService, public loadingCtrl: LoadingController) {
 
@@ -25,8 +34,73 @@ export class MarketPage {
   ionViewDidLoad() {
 
     this.loading.present();
+    var $this = this;
+    /*获取tab标签*/
+    var tabBarUrl = './assets/data/index/tabBar.json';
+    this.marketService
+      .getData(tabBarUrl, '')
+      .then(function (data) {
+        $this.tabBarData = data.itemsObj;
+        /*获取列表内容*/
+        $this.listGet((data.itemsObj)[0].id, 1, '')
+      });
+
     this.loading.dismiss();
     console.log('ionViewDidLoad MarketPage');
+  }
+
+  listGet(id, page, fn) {
+
+    var $this = this;
+    /*点击获取列表内容*/
+    var listUrl = './assets/data/market.json';
+    var param = {"page": page, "id": id}
+    this.marketService
+      .getData(listUrl, param)
+      .then(function (data) {
+        fn && fn.call($this);
+        $this.slidesImg = data.items;
+        $this.listData = $this.listData.concat(data.itemsList);
+        $this.isTabActive = id;
+        $this.pageNum = page;
+        console.log('当前分页', page)
+      });
+  }
+
+  /*刷新获取*/
+  doRefresh(refresher) {
+    this.pageNum = this.pageNum + 1;
+    this.listGet(this.isTabActive, this.pageNum, function () {
+      refresher.complete();
+    })
+  }
+
+  /*点击获取*/
+  clickGet(id) {
+    var $this = this;
+    this.listGet(id, 1, function () {
+      $this.listData = []
+      $this.isShowSlide = false
+    })
+  }
+
+  /*弹出点击获取*/
+  clickPopGet(id, index) {
+    var $this = this;
+    this.listGet(id, 1, function () {
+      $this.listData = []
+      $this.isShowSlide = false
+      $this.goToSlide(index)
+    })
+  }
+
+  /*设置slides 当前焦点*/
+  goToSlide(index) {
+    this.slides.slideTo(index, 500);
+  }
+
+  goDetail(id) {
+    this.navCtrl.push(GoodsDetailsPage, {"id": id});
   }
 
 }
